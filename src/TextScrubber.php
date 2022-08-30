d58ab8be367785b0b1325f434fffdbdbbe4527ba
George Nikides <gnikides@yahoo.com>
2022-08-30 16:53:23 +0200

    Update

--- src/TextScrubber.php
+++ src/TextScrubber.php
@@ -93,7 +93,7 @@ class TextScrubber
             ' ',
             $string
         );
-        $token = str_ireplace(
+        $token = str_replace(
             [
                 'a','b','c','d','e','f','g','h','i','j','k','l','m','n',
                 'o','p','q','r','s','t','u','v','w','x','y','z'                                
