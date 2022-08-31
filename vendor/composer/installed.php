c7beb5c4cc15e47f9584534fe566c71e09ffbe3a
George Nikides <gnikides@yahoo.com>
2022-09-01 00:19:09 +0200

    Update

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '24b11b5592e81ab91bb6fff01aa7aac5517f8646',
+        'name' => 'gnikides/lemma',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'gnikides/lemma' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '24b11b5592e81ab91bb6fff01aa7aac5517f8646',
+            'dev_requirement' => false,
+        ),
+        'league/csv' => array(
+            'pretty_version' => '9.7.4',
+            'version' => '9.7.4.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../league/csv',
+            'aliases' => array(),
+            'reference' => '002f55f649e7511710dc7154ff44c7be32c8195c',
+            'dev_requirement' => false,
+        ),
+    ),
+);
